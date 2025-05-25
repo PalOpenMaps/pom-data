710672eca34b3071bb61ecf1ab87dd54129ce9a6
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-05-25 20:59:25 +0100

    data update with poha

--- scripts/make-places.js
+++ scripts/make-places.js
@@ -1,12 +1,12 @@
-import { readTXT, writeJSON } from 'https://deno.land/x/flat@0.0.15/mod.ts';
-import { csvParse, csvFormat, autoType } from "https://unpkg.com/d3-dsv@3.0.1/src/index.js";
+import { readTXT, readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.15/mod.ts';
+import { csvParse, autoType } from "https://unpkg.com/d3-dsv@3.0.1/src/index.js";
 import { locs_path, places_path, redirect_path } from "./config.js";
 
 const locs = csvParse(await readTXT(locs_path), autoType);
 
 // Make geojson for all places
 const geojson = {type: "FeatureCollection", features: []};
-const redirect = [];
+const redirect = {};
 
 for (const loc of locs.filter(d => !["Sinai", "Quneitra"].includes(d["district_1945"]) && d["lng"])) {
   const geometry = {type: "Point", coordinates: [loc["lng"], loc["lat"]]};
@@ -28,16 +28,20 @@ for (const loc of locs.filter(d => !["Sinai", "Quneitra"].includes(d["district_1
     else if (props[key]) properties[key] = props[key];
   }
   geojson.features.push({type: "Feature", geometry, properties});
-  redirect.push({id_old: props.id_old, id_new: props.id, slug: props.slug})
+  if (props.id_old) redirect[props.id_old] = {id_new: props.id, slug: props.slug};
   
   const path = `data/places/${loc.slug}.json`;
-  // const poha_path = `raw-data/poha/${loc.slug}.json`;
-  // if (existsSync(poha_path)) props.poha =  JSON.parse(readFileSync(poha_path, {encoding:'utf8', flag:'r'}));
+  const poha_path = `raw-data/poha/${loc.slug}.json`;
+  try {
+    props.poha = await readJSON(poha_path);
+  } catch (err) {
+    console.log(`No data at ${poha_path}`);
+  }
   writeJSON(path, {type: "Feature", geometry, properties: props});
   console.log(`Wrote ${path}`);
 }
 writeJSON(places_path, geojson);
 console.log(`Wrote ${places_path}`);
 
-// writeFileSync(redirect_path, csvFormat(redirect.filter(r => r.id_old)));
-// console.log(`Wrote ${redirect_path}`);
+writeJSON(redirect_path, redirect);
+console.log(`Wrote ${redirect_path}`);
