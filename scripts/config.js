ca426c19a6b74b5528a592f98713dc18374c0c78
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-05-25 21:18:46 +0100

    added make-config script and fixed curl

--- scripts/config.js
+++ scripts/config.js
@@ -3,3 +3,4 @@ export const places_path = "data/places.json";
 export const redirect_path = "data/redirects.json";
 export const layers_path = "data/layers.json";
 export const sheets_path = "data/sheets.json";
+export const config_path = "data/config.json";
---
+++ scripts/make-config.js
@@ -0,0 +1,37 @@
+import { readTXT, writeJSON } from 'https://deno.land/x/flat@0.0.15/mod.ts';
+import { csvParse, autoType } from "https://unpkg.com/d3-dsv@3.0.1/src/index.js";
+import { config_path } from "./config.js";
+
+const keys = ["authors", "groups", "layers", "sources", "statuses", "translations"];
+const parseForBoolean = (val) => val === "TRUE" ? true : val === "FALSE" ? false : val;
+
+const config = {};
+
+for (const key of keys) {
+  const path = `raw-data/${key}.csv`;
+  console.log(`Reading ${path}`);
+  const data = csvParse(await readTXT(path), autoType);
+
+  config[key] = data.map(d => {
+		const row = {};
+		for (const col of data.columns) {
+			const arr = col.match(/(?<=\[)\d*(?=\])/);
+			if (Array.isArray(arr)) {
+				const key = col.split("[")[0];
+				if (arr[0] === "") row[key] = d[col].split(", ");
+				else {
+					if (!row[key]) row[key] = [];
+					row[key][arr[0]] = parseForBoolean(d[col]);
+				}
+			} else row[col] = parseForBoolean(d[col]);
+		}
+		const newCols = Object.keys(row);
+		for (const col of newCols) {
+			if (Array.isArray(row[col]) && row[col].every(d => !d)) row[col] = null;
+		}
+		return row;
+	});
+}
+
+writeJSON(config_path, config);
+console.log(`Wrote ${config_path}`);
