67edcc86dc9c275f8489f113b0270537bbdc388a
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-05-30 10:56:35 +0100

    changed processing of sheets to array

--- scripts/make-config.js
+++ scripts/make-config.js
@@ -4,6 +4,29 @@ import { config_path, sheets_path } from "./config.js";
 
 const keys = ["authors", "groups", "layers", "sources", "statuses", "translations", "pages", "sheets"];
 const parseForBoolean = (val) => val === "TRUE" ? true : val === "FALSE" ? false : val;
+const parseRow = (d, cols = null)=> {
+	if (!cols) cols = Object.keys(row);
+
+	const row = {};
+
+	for (const col of cols) {
+		const arr = col.match(/(?<=\[)\d*(?=\])/);
+		if (Array.isArray(arr)) {
+			const key = col.split("[")[0];
+			if (arr[0] === "") row[key] = d[col].split(", ");
+			else {
+				if (!row[key]) row[key] = [];
+				row[key][arr[0]] = parseForBoolean(d[col]);
+			}
+		} else row[col] = parseForBoolean(d[col]);
+	}
+	const newCols = Object.keys(row);
+	for (const col of newCols) {
+		if (Array.isArray(row[col]) && row[col].every(d => !d)) row[col] = null;
+	}
+	
+	return row;
+}
 
 const config = {};
 
@@ -13,34 +36,17 @@ for (const key of keys) {
   console.log(`Reading ${path}`);
   const data = csvParse(await readTXT(path), autoType);
 
-  const obj = {};
-
-  for (const d of data) {
-		const row = {};
-		for (const col of data.columns) {
-			const arr = col.match(/(?<=\[)\d*(?=\])/);
-			if (Array.isArray(arr)) {
-				const key = col.split("[")[0];
-				if (arr[0] === "") row[key] = d[col].split(", ");
-				else {
-					if (!row[key]) row[key] = [];
-					row[key][arr[0]] = parseForBoolean(d[col]);
-				}
-			} else row[col] = parseForBoolean(d[col]);
-		}
-		const newCols = Object.keys(row);
-		for (const col of newCols) {
-			if (Array.isArray(row[col]) && row[col].every(d => !d)) row[col] = null;
-		}
-		
-    const rowKey = row.name_en || row.en;
-    obj[rowKey] = row;
-	}
-	
 	if (key === "sheets") {
-		writeJSON(sheets_path, Object.values(obj));
+		writeJSON(sheets_path, data.map(d => parseRow(d, data.columns)));
 		console.log(`Wrote ${sheets_path}`);
 	} else {
+		const obj = {};
+
+		for (const d of data) {
+			const row = parseRow(d, data.columns);
+			const rowKey = row.name_en || row.en;
+			obj[rowKey] = row;
+		}
 		config[key] = obj;
 	}
 }
